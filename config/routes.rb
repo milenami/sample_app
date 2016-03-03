ceb57d80003d183e15698f140f19cc87a583a2b4
milenami <mihajlovic.milena@gmail.com>
2016-03-03 12:16:33 +0000

    prvi i drugi zadatak

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
   
-  get 'static_pager/home'
+  root 'static_pager#home'
   get 'static_pager/help'
   get 'static_pager/about'
+  get 'static_pager/contact'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
