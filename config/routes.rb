934aa4242663e746adc42950c0377123a98cfe7b
milenami <mihajlovic.milena@gmail.com>
2016-03-11 14:51:52 +0000

    dodala sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   
   
-  root 'static_pager#home'
-  get 'static_pager/help'
-  get 'static_pager/about'
-  
+Rails.application.routes.draw do
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
