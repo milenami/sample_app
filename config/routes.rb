28f3c0224f6bdc99d2f5639b1dc3e8706b4f285c
milenami <mihajlovic.milena@gmail.com>
2016-03-14 12:27:20 +0000

    promene: test site layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   
   
 Rails.application.routes.draw do
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  root             'static_pager#home'
+  get 'help'    => 'static_pager#help'
+  get 'about'   => 'static_pager#about'
+  get 'contact' => 'static_pager#contact'
   get 'signup'  => 'users#new'
 end
 
